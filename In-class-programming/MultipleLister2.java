f86128b0a466535e6b0d59d487af4900eb91ad96
OOFA77 <beshoybassem1@gmail.com>
2025-10-23 19:04:39 +0200

    Home additions

---
+++ In-class-programming/MultipleLister2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MultipleLister2
+{
+    public static void main(String[] args)
+    {
+
+
+        Scanner scanner = new Scanner(System.in);
+        int s = scanner.nextInt();
+        System.out.println(s);
+    }
+
+
+}
